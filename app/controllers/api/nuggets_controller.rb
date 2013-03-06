4beaead52f9dc4faa48dfff44864866ce77687ec
John Quarto-vonTivadar <jcquarto@gmail.com>
2013-03-06 11:09:36 -0600

    pushed email processing out of Nugget model and into API controller

--- app/controllers/api/nuggets_controller.rb
+++ app/controllers/api/nuggets_controller.rb
@@ -5,12 +5,49 @@ class Api::NuggetsController < ApplicationController
     # make sure the thing posting has rights to post here... maybe with
     # http basic auth or a super secret token
 
-    n = Nugget.create_from_postmark(Postmark::Mitt.new(request.body.read))
+    #n = Nugget.create_from_postmark(Postmark::Mitt.new(request.body.read))
+    message= Postmark::Mitt.new(request.body.read)
+    # ignore messages with no attachments
+    if message.attachments.empty?
+      logger.warning "message #{message.message_id} from #{messager.from}: no attachments!; skipping."
+      return
+    end
 
-    # do a bunch of stuff like send an email back if the user can't be
-    # found, doesn't exist, etc.
+    message.attachments.each {|attachment, i|
+      #logger.info "message #{message.message_id}: reading attachment #{i} ('#{attachment.file_name}') of type #{attachment.content_type}"
+      begin
+        signage = SignageUploader.new
+        signage.cache!(attachment.read)
+      rescue CarrierWave::IntegrityError => e
+        # puts there was a problem storing attachments
+        logger.error "message #{message.message_id}: problem storing attachment #{i} ('#{attachment.file_name}') of message #{message.message_id}"
+        logger.error [e, *e.backtrace].join("\n")
+        return
+      end
 
-    render :text => "Created a Postmark nugget", :status => :created
+      n = Nugget.new(
+        submitter: message.from,
+        submission_method: "email",
+        submitted_at: Time.now
+      )
+
+      jpg = EXIFR::JPEG.new(signage.file.path)
+      if jpg.gps.compact.blank?
+        logger.warn "NO GPS FOUND!"
+        n.no_gps!
+        # note that when there's no GPS we don't even bother to save the file
+      else
+        n.latitude = jpg.gps[0]
+        n.longitude = jpg.gps[1]
+        n.signage = signage
+        n.process_geodata
+        n.signage_reviewable!
+      end
+
+      logger.error "message #{message.message_id} from #{message.from}: nugget created"
+    }
+
+    render :text => "yay", :status => :created
   end
 
   private
--- app/models/nugget.rb
+++ app/models/nugget.rb
@@ -2,8 +2,9 @@ class Nugget < ActiveRecord::Base
   mount_uploader :signage, SignageUploader
 
   reverse_geocoded_by :latitude, :longitude, {address: :signage_address}
-  before_save :default_values, :process_geodata
-  
+  before_save :default_values
+  #before_save :process_geodata
+
   attr_accessible :latitude, :longitude
   attr_accessible :submission_method, :submitted_at, :submitter
   attr_accessible :state
@@ -69,62 +70,55 @@ class Nugget < ActiveRecord::Base
     self.submitter ||= "Cato"
   end
 
-  def self.create_from_postmark(message)
-    # ignore messages with no attachments
-    if message.attachments.empty?
-      logger.info "message #{message.message_id}: no attachments!; skipping."
-      return
+  # def self.create_from_postmark(message)
+  #   # ignore messages with no attachments
+  #   if message.attachments.empty?
+  #     logger.info "message #{message.message_id}: no attachments!; skipping."
+  #     return
+  #   end
+
+  #   message.attachments.each {|attachment, i|
+  #     logger.info "message #{message.message_id}: reading attachment #{i} ('#{attachment.file_name}') of type #{attachment.content_type}"
+  #     begin
+  #       signage = SignageUploader.new
+  #       signage.cache!(attachment.read)
+  #     rescue CarrierWave::IntegrityError => e
+  #       # puts there was a problem storing attachments
+  #       logger.error "message #{message.message_id}: problem storing attachment #{i} ('#{attachment.file_name}') of message #{message.message_id}"
+  #       logger.error [e, *e.backtrace].join("\n")
+  #       return
+  #     end
+
+  #     n = Nugget.new(
+  #       submitter: message.from,
+  #       submission_method: "email",
+  #       submitted_at: Time.now
+  #     )
+
+  #     jpg = EXIFR::JPEG.new(signage.file.path)
+  #     if jpg.gps.compact.blank?
+  #       logger.warn "NO GPS FOUND!"
+  #       n.no_gps!
+  #       # note that when there's no GPS we don't even bother to save the file
+  #     else
+  #       n.latitude = jpg.gps[0]
+  #       n.longitude = jpg.gps[1]
+  #       n.signage = signage
+  #       n.signage_reviewable!
+  #     end
+  #   }
+  # end
+
+  def process_geodata
+    #if latitude_changed? or longitude_changed?
+    #end
+    res = Geocoder.search(latlong)
+    if res.present?
+      populate_address(res.first)
     end
-
-    message.attachments.each {|attachment, i|
-      logger.info "message #{message.message_id}: reading attachment #{i} ('#{attachment.file_name}') of type #{attachment.content_type}"
-      begin
-        signage = SignageUploader.new
-        signage.cache!(attachment.read)
-      rescue CarrierWave::IntegrityError => e
-        # puts there was a problem storing attachments
-        logger.error "message #{message.message_id}: problem storing attachment #{i} ('#{attachment.file_name}') of message #{message.message_id}"
-        logger.error [e, *e.backtrace].join("\n")
-        return
-      end
-
-      n = Nugget.new(
-        submitter: message.from,
-        submission_method: "email",
-        submitted_at: Time.now
-      )
-
-      jpg = EXIFR::JPEG.new(signage.file.path)
-      if jpg.gps.compact.blank?
-        logger.warn "NO GPS FOUND!"
-        n.no_gps!
-        # note that when there's no GPS we don't even bother to save the file
-      else
-        n.latitude = jpg.gps[0]
-        n.longitude = jpg.gps[1]
-        n.signage = signage
-        n.signage_reviewable!
-      end
-    }
   end
 
   private
-  def process_geodata
-    if latitude_changed? or longitude_changed?
-      res = load_geodata
-      if res.present?
-        populate_address(res)
-      end
-    end
-  end
-
-  def load_geodata
-    res = Geocoder.search(latlong)
-    if res.present?
-      res.first
-    end
-  end
-
   def populate_address(geo_addr)
     self.signage_city = geo_addr.city
     self.signage_state = geo_addr.state
