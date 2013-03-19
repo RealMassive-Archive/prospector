07b222e109f2e4acb9e857c19b52956b662983f9
John Quarto-vonTivadar <jcquarto@gmail.com>
2013-03-18 23:44:54 -0500

    rotate signage based on orientation info in metadata

--- app/uploaders/signage_uploader.rb
+++ app/uploaders/signage_uploader.rb
@@ -45,8 +45,8 @@ class SignageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  #process :fix_exif_rotation
-  #process :strip
+  process :fix_exif_rotation
+  process :strip
 
   # Create different versions of your uploaded files:
   # version :thumb do
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,33 +6,33 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => 'AKIAJNYGBKJOOKTZDGJQ',
-      :aws_secret_access_key  => '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',
+      :aws_secret_access_key  => 'WQkn20y2jlpn77K9JSu80x6+vPVfkbvCtUji4PHk'
     }
     config.fog_directory  = 'realmassive_nuggets'
   end
 end
 
-# module CarrierWave
-#   module MiniMagick
+module CarrierWave
+  module MiniMagick
 
-#     # Rotates the image based on the EXIF Orientation
-#     def fix_exif_rotation
-#       manipulate! do |img|
-#         img.auto_orient  #note: if switching to RMagick, make this "auto_orient!" (bang at end)
-#         img = yield(img) if block_given?
-#         img
-#       end
-#     end
+    # Rotates the image based on the EXIF Orientation
+    def fix_exif_rotation
+      manipulate! do |img|
+        img.auto_orient  #note: if switching to RMagick, make this "auto_orient!" (bang at end)
+        img = yield(img) if block_given?
+        img
+      end
+    end
 
-#     # Strips out all embedded information from the image
-#     def strip
-#       manipulate! do |img|
-#         img.strip!
-#         img = yield(img) if block_given?
-#         img
-#       end
-#     end
+    # Strips out all embedded information from the image
+    def strip
+      manipulate! do |img|
+        img.strip!
+        img = yield(img) if block_given?
+        img
+      end
+    end
 
-#   end
-# end
+  end
+end
 
