c1ca6fc2b68df3ca5f6e1b775d752008887831a5
John Quarto-vonTivadar <jcquarto@gmail.com>
2013-03-19 00:02:33 -0500

    rotate signage based on orientation info in metadata

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,8 @@ CarrierWave.configure do |config|
       :aws_access_key_id      => 'AKIAJNYGBKJOOKTZDGJQ',
       :aws_secret_access_key  => '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'
     }
-    config.fog_directory  = 'realmassive_nuggets'
+    config.fog_directory  = 'realmassive_nuggets',
+    config.fog_host = "http://#{config.fog_directory}.s3.amazonaws.com"
   end
 end
 
