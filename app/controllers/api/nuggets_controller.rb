c3fa61bc23dfef5024a1fb56632acb952e73dd58
John Quarto-vonTivadar <jcquarto@gmail.com>
2013-05-30 15:27:15 -0500

    tweaking TogetherMobile submissions to plain text

--- app/controllers/api/nuggets_controller.rb
+++ app/controllers/api/nuggets_controller.rb
@@ -33,7 +33,7 @@ class Api::NuggetsController < ApplicationController
       together_hash = message.mailbox_hash.split('--together_id--')
       together_id = together_hash.last
       submitter = together_hash.first.gsub('--at--', '@')
-      together_url = "https://test.togethermobile.com/export/verify?job_id="
+      together_url = "https://test.togethermobile.com/export/verify.json?job_id="
       begin
         together_response = HTTParty.get(together_url + together_id)
       rescue
