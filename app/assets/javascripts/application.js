7966d83af8cd1c51e734337de4e96d61e5bd3123
deepak <deepak@geek.(none)>
2013-08-22 17:09:50 +0530

    jquery ui gem fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,10 @@
 //= require bootstrap-editable
 //= require bootstrap-tab
 // or use this: require bootstrap-editable-inline   for inline editing
+//= require jquery.ui.core
+//= require jquery.ui.widget
+//= require jquery.ui.mouse
+//= require jquery.ui.position
 //= require jquery.ui.draggable
 //= require jquery.ui.droppable
 //= require_tree .
