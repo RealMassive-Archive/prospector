cc5124e30f1a916e2d21d22196270895223b58a9
J. Austin Hughey <jaustinhughey@me.com>
2014-01-03 21:49:08 -0600

    Use cheap authentication to protect app from being wide open to the world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :cheap_authentication # See rationale in method comments
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
@@ -14,6 +15,26 @@ class ApplicationController < ActionController::Base
 
   def render_not_found
     render :template => "/errors/404"
+  end
+
+private
+
+  #
+  # cheap_authentication
+  #
+  # The point of this method is to prevent total access to the app from
+  # the public at large. The problem is that, as of 3 Dec 2014, the app
+  # is built to let anybody sign up and start administering the queue.
+  # There are plans to re-write this app in the future, so instead of
+  # investing lots of time and money in rebuilding this to be more
+  # "correct", this is just a cheap way to get some basic pseudo-security
+  # so at least nobody's pants are down. It's a pair of cheap jeans, not
+  # kevlar body armor.
+  #
+  def cheap_authentication
+    authenticate_or_request_with_http_basic "RealMassive Prospector" do |u,p|
+      u == ENV['CHEAP_AUTH_USERNAME'] && p == ENV['CHEAP_AUTH_PASSWORD']
+    end
   end
 
 end
