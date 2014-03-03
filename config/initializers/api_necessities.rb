387ef88e98d5a7507cf634e8d28895dce6eaf1e0
J. Austin Hughey <jaustinhughey@me.com>
2014-03-03 01:21:07 -0600

    Provide default ENV vars for API connections

--- config/initializers/api_necessities.rb
+++ config/initializers/api_necessities.rb
@@ -1,1 +1,18 @@
+# Load all the API-specific models
 Dir["#{Rails.root}/app/models/api/*.rb"].each { |f| require f }
+
+# Get or set environment variables needed for communication with the API.
+# The defaults supplied herein are for basic auth against the Electrick-co
+# API and are only for staging. Production values will be specified by
+# environment variables.
+defaults = {
+  electrick_api_endpoint: 'https://realmassive-staging.appspot.com',
+  electrick_api_username: 'prospector',
+  electrick_api_password: '12345'
+}
+
+defaults.each do |env_var, val|
+  unless ENV[env_var.to_s.upcase]
+    ENV[env_var.to_s.upcase] = defaults[env_var]
+  end
+end
