0d3e166bcfd7a004c4f79af9400e5cc1e9ec6195
J. Austin Hughey <jaustinhughey@me.com>
2014-03-03 03:06:00 -0600

    major refactor of api/building to use ApiWrapper class based on Typhoeus

---
+++ app/models/api/api_wrapper.rb
@@ -0,0 +1,36 @@
+#
+# Wrapper class that uses Typhoeus (https://github.com/typhoeus/typhoeus)
+# to perform API requests against the Electrick API.
+#
+class ApiWrapper
+
+  #
+  # ApiWrapper.get('/api/v1/buildings', {address: {...}} )
+  #   path: the path to run the GET against
+  #   params: the request params to throw. Should be a hash when passed in.
+  #
+  def self.get(path, params)
+    response = Typhoeus::Request.new(
+      ENV['ELECTRICK_API_ENDPOINT'] + path,
+      method: :get,
+      params: params,
+      userpwd: [ENV['ELECTRICK_API_USERNAME'],ENV['ELECTRICK_API_PASSWORD']].join(':')
+    )
+    return response.run
+  end
+
+  #
+  # ApiWrapper.post('/api/v1/buildings', {address: {...}})
+  #   path: The path you're going to POST to
+  #   body: POST request body
+  #
+  def self.post(path, body)
+    response = Typhoeus::Request.new(
+      ENV['ELECTRICK_API_ENDPOINT'] + path,
+      method: :post,
+      body:   body,
+      userpwd: [ENV['ELECTRICK_API_USERNAME'],ENV['ELECTRICK_API_PASSWORD']].join(':')
+    )
+    return response.run
+  end
+end
--- app/models/api/building.rb
+++ app/models/api/building.rb
@@ -10,55 +10,38 @@
 #
 
 class Building
-  attr_accessor :name
-  attr_accessor :type
-  attr_accessor :subtype
-  attr_accessor :size
-  attr_accessor :size_units
-  attr_accessor :lot_size
-  attr_accessor :lot_size_units
-  attr_accessor :building_class
-  attr_accessor :year_built
-  attr_accessor :highlights
-  attr_accessor :address
-
-  attr_reader   :key
-  attr_reader   :manager
-  attr_reader   :created_by
-  attr_reader   :attachments
-
-  def initialize(opts={})
-    # Step 1: Find and set key, manager, created_by and attachments.
-    # Nil objects are entirely acceptable here if not found in opts{}.
-    @key = "TODO"
-    @manager = "TODO"
-    @created_by = "TODO"
-    @attachments = "TODO"
-
-    # Step 2: Loop through opts{} and see if we have a method that
-    # responds to that particular parameter, and if so, set it to the value.
-    # TODO
-  end
-
-  def self.create(opts={}.with_indifferent_access)
-    # Start by checking for all required options to create a new Building object.
-    unless opts[:name] && opts[:type] && opts[:subtype] && opts[:size] && opts[:size_units] && opts[:lot_size] && opts[:lot_size_units] && opts[:building_class] && opts[:year_built] && opts[:highlights] && opts[:address]
-      raise ArgumentError, "You must supply all editable properties as described at https://github.com/electrik-co/realmassive/wiki/Building-API."
+  #
+  # create({street: "...", city: "..." ...})
+  # Creates a building. Returns the response object.
+  # Parameters:
+  #   address: Hash containing street, city, state, zipcode keys
+  #
+  def self.create(address={}.with_indifferent_access)
+    # Error checking - make sure we have what we need per the Electrick-co API
+    # https://github.com/electrik-co/realmassive/wiki/Building-API
+    if address.blank?
+      raise ArgumentError, "The address hash is blank."
     end
 
-    # Check to be sure address contains what we need
-    unless opts[:address][:street] && opts[:address][:city] && opts[:address][:state] && opts[:address][:zipcode]
-      raise ArgumentError, "You did not supply street, city, state and zipcode in your address hash."
+    # Address must have ALL of the following keys
+    [:street, :city, :state, :zipcode].each do |k|
+      unless address[k] || address[k].blank?
+        raise ArgumentError, "address[:#{k}] is blank and shouldn't be"
+      end
     end
 
-    # Make sure state is exactly a two-letter abbreviation
-    unless opts[:address][:state].length == 2
-      raise ArgumentError, "State must be a 2-letter abbreviation."
+    if address[:state].length != 2
+      raise ArgumentError, "address[:state] must be an exact two-letter state abbreviation."
     end
+
 
 
 
     # Shove it up there.
+    json = ::Yajl::Encoder.encode(opts)
+
+
+
   end
 
   #
@@ -66,27 +49,29 @@ class Building
   # Utilizes the Electrick-co API to fetch all buildings by its search
   # functionality and returns an array of hashes with indifferent access.
   #
-  def self.find(address, city, state, zipcode)
+  def self.search(address={}.with_indifferent_access)
     # Error checking - make sure we have what we need per the Electrick-co API
     # https://github.com/electrik-co/realmassive/wiki/Building-API
-    if address.blank? || city.blank? || state.blank? || state.length != 2 || zipcode.blank?
-      raise ArgumentError, "You must supply ALL parameters, and state must be a two-letter abbreviation."
+    if address.blank?
+      raise ArgumentError, "The address hash is blank."
     end
 
-    conn = Excon.new((ENV['ELECTRIC_API_ENDPOINT'] || 'https://realmassive-staging.appspot.com') + "/api/v1/buildings?address")
-    response = conn.get(query: { street: address, city: city, state: state, zipcode: zipcode.to_s, limit: 3 })
-    new_arr = []
-    parsed = ::Yajl::Parser.new.parse(response.body)
-    if parsed["results"]
-      parsed["results"].each do |x|
-        new_arr << x.with_indifferent_access
+    # Address must have ALL of the following keys
+    [:street, :city, :state, :zipcode].each do |k|
+      unless address[k] || address[k].blank?
+        raise ArgumentError, "address[:#{k}] is blank and shouldn't be"
       end
-      return new_arr
-    else
-      return [] # empty array if there aren't any buildings found
     end
 
+    if address[:state].length != 2
+      raise ArgumentError, "address[:state] must be an exact two-letter state abbreviation."
+    end
+
+    # Perform GET request to retrieve results
+    results = ApiWrapper.get('/api/v1/buildings', {address: address, limit: 3})
+
+    # Return the actual buildings that the API gave us
+    return Yajl::Parser.parse(results.body)['results']
 
   end
-
 end
